88df028eae0a55139e5b04622c56c8b1805795ca
Dhiooonn <diondaman51@gmail.com>
2025-09-01 11:20:02 +0700

    repair animation

--- src/app/components/Animasi/FadeInWhenVisible.tsx
+++ src/app/components/Animasi/FadeInWhenVisible.tsx
@@ -15,7 +15,7 @@ export default function FadeInWhenVisible({
     <motion.div
       ref={ref}
       initial={{ opacity: 0, y: 40 }}
-      animate={inView ? { opacity: 1, y: 0 } : {}}
+      animate={inView ? { opacity: 1, y: 0 } : { opacity: 1, y: 0 }}
       transition={{ duration: 0.6, ease: "easeOut" }}
     >
       {children}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,12 +12,15 @@ export default function AllProjects() {
             <Navbar />
             <Butterfly />
 
+
             <section className="max-w-6xl mx-auto pt-24 px-4 mb-12 sm:px-6">
                 <FadeInWhenVisible>
                     <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-10 sm:mb-14 text-center font-fredoka text-teal-500">
                         ALL PROJECTS
                     </h1>
+                </FadeInWhenVisible>
 
+                <FadeInWhenVisible>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl mx-auto font-poppins">
                         {projects.map((project: Project) => (
                             <ProjectCard key={project.id} project={project} />
@@ -27,6 +30,6 @@ export default function AllProjects() {
             </section>
 
             <Footer />
-        </div>
+        </div >
     );
 }
